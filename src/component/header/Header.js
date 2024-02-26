b1c091ac2b3ca51a102b4665a039cdd9ee9a0cf1
Louis Blonde <loul0uboypy@gmail.com>
2024-02-26 23:02:33 +0100

    update header.js

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,9 +1,65 @@
 import React from "react";
+import { useState } from "react";
+import Menu from "../menu/Menu";
 
 const Header = () => {
+  const [search, setSearch] = useState("");
+
+  const searchHandler = (event) => {
+    setSearch(event.target.value);
+  };
   return (
     <div className="header">
-      <input type="search" placeholder="Search a fighter..." />
+      <div>
+        <input
+          type="search"
+          value={search}
+          onChange={searchHandler}
+          placeholder="Search a fighter..."
+        />
+        <input type="button" value="Search" />
+      </div>
+      <div>
+        <div className="genderList">
+          <Menu options={["All", "Man", "Woman"]} />
+        </div>
+        <Menu
+          options={
+            sexe === "All"
+              ? [
+                  "All",
+                  "Strawweight",
+                  "Flyweight",
+                  "Bantamweight",
+                  "Featherweight",
+                  "Lightweight",
+                  "Welterweight",
+                  "Middleweight",
+                  "Light heavyweight",
+                  "Heavyweight",
+                ]
+              : sexe === "Woman"
+              ? [
+                  "All",
+                  "Strawweight",
+                  "Flyweight",
+                  "Bantamweight",
+                  "Featherweight",
+                ]
+              : [
+                  "all",
+                  "Flyweight",
+                  "Bantamweight",
+                  "Featherweight",
+                  "Lightweight",
+                  "Welterweight",
+                  "Middleweight",
+                  "Light heavyweight",
+                  "Heavyweight",
+                ]
+          }
+        />
+      </div>
     </div>
   );
 };
---
+++ src/component/menu/Menu.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useState } from "react";
+
+const Menu = ({ options }) => {
+  const option = options;
+  const [open, setOpen] = useState(false);
+  const [valueDefault, setValueDefault] = useState(option[0]);
+
+  const openUp = () => {
+    setOpen(!open);
+  };
+  const selectioned = (i) => {
+    setOpen(false);
+    setValueDefault(option[i]);
+  };
+  return (
+    <div>
+      <div onClick={openUp}>{valueDefault}</div>
+      {open && (
+        <ul className="scrollContainer">
+          {option.map((element, index) => {
+            return (
+              <span key={index}>
+                <div className="containerLi">
+                  <li
+                    className="scrollMenuLi"
+                    onClick={() => selectioned(index)}
+                  >
+                    {element}
+                  </li>
+                </div>
+              </span>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Menu;
