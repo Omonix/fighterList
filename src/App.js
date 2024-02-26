db76144c22c7c9dc8f748c926a0aa8766d9cebe2
Louis Blonde <loul0uboypy@gmail.com>
2024-02-26 21:29:34 +0100

    create component folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Header from "./component/header/Header.js";
+import Main from "./component/main/Main.js";
+import Footer from "./component/footer/Footer.js";
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
