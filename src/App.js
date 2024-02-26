bfc838f9fd30f198448b95c1bf9b953c989adffa
Louis Blonde <loul0uboypy@gmail.com>
2024-02-26 22:01:35 +0100

    create router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./component/header/Header.js";
 import Main from "./component/main/Main.js";
 import Footer from "./component/footer/Footer.js";
+import Error from "./component/error/Error.js";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="app">
       <Header />
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer />
     </div>
   );
